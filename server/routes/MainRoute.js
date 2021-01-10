3cc6b78d4d3366eafa45aff86aa619bb02bb2932
Peter Badinka <peter4478@gmail.com>
2021-01-10 04:51:10 +0100

    upg...

--- server/routes/MainRoute.js
+++ server/routes/MainRoute.js
@@ -7,6 +7,11 @@ const
 	jsonParser = bodyParser.json(),
 	Route = express.Router()
 
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*")
+		next()
+	})
+
 //=======================================================================
 // Midleware
 //=======================================================================
